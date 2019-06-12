0cce99c1397cf98476a88794310f4a1968be558f
Lance Adriano <lancegabriel.adriano.2873@mail.linnbenton.edu>
2019-06-12 01:36:03 -0700

    .

--- test_Goku.py
+++ test_Goku.py
@@ -1,8 +1,25 @@
 #! /usr/bin/env python3
 import pytest
 
-from Goku import update_stats
+from Goku import update_stats, read_toker, read_server_id, on_member_join, on_message, on_ready
 
 
 def test_update_stats():
-    assert pytest.raises(Exception) != 0
+    with pytest.raises(Exception):
+        update_stats()
+
+
+def test_read_token():
+    assert line == f.readlines
+
+
+def test_read_server_id():
+    assert line == f.readlines
+
+
+def test_on_member_join():
+    assert 1 == 1
+
+
+def test_on_message():
+    assert id == client.get_guild(serverid)
